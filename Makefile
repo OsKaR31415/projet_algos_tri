a6ca3cd7cf2cbd870c56fb2768e341caff937d4b
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2023-05-04 04:12:44 +0200

    tests for algorithms

--- Makefile
+++ Makefile
@@ -8,5 +8,6 @@ tests:
 	python3 -m doctest node.py
 	python3 -m doctest linked_list.py
 	python3 -m doctest utils.py
+	python3 -m doctest algorithms.py
 
 
--- algorithms.py
+++ algorithms.py
@@ -4,6 +4,15 @@ from node import Node
 from utils import *
 
 def recursive_insertion(lst: List, value) -> List:
+    """
+    Tests:
+        >>> recursive_insertion(List(1), 2)
+        List(1, 2)
+        >>> recursive_insertion(List(1), 0)
+        List(0, 1)
+        >>> all(is_sorted(recursive_insertion(iota(50), n)) for n in range(51))
+        True
+    """
     if lst.is_empty() or lst.value > value:
         lst.prepend(value)
         return lst
@@ -11,18 +20,28 @@ def recursive_insertion(lst: List, value) -> List:
     return lst
 
 def iterative_insertion(lst: List, value) -> List:
-    new_list = List(True)  # List containing a foo value
+    """
+    Tests:
+        >>> iterative_insertion(List(1), 2)
+        List(1, 2)
+        >>> iterative_insertion(List(1), 0)
+        List(0, 1)
+        >>> all(is_sorted(iterative_insertion(iota(50), n)) for n in range(51))
+        True
+    """
+    new_list = List(False)  # List containing a foo value
     tail = new_list  # Last node of the list
     lst_ptr = lst  # pointer to the current element of lst
     # /!\ order of conditions in the while is important :
     # you need to test that lst_ptr is not empty before getting its value.
     while (not lst_ptr.is_empty()) and (value > lst_ptr.value):
+        # copy the current lst_ptr.value at the end of the new list
         tail.next = List(lst_ptr.value)
         tail = tail.next
         lst_ptr = lst_ptr.next
     tail.next = List(value)
     tail.next.next = lst_ptr
-    return new_list.cdr()
+    return new_list.cdr()  # skip the useless first element
 
 
 def insertion_sort(insertion_function: Callable[List, List], lst: List) -> List:
@@ -47,10 +66,11 @@ def time_recursive_insertion_sort(sample: List) -> float:
     recursive_insertion_sort(sample)
 
 
-A = random_list(10)
-# print(recursive_insertion_sort(A))
-# print(A)
-# print(iterative_insertion(iota(5), 6))
-print(is_sorted(iterative_insertion_sort(A)))
+# for num_values in (10, 100, 1000):
+#     R = random_list(num_values)
+#     print(time_recursive_insertion_sort(R))
+#     print(time_iterative_insertion_sort(R))
+
+# time_recursive_insertion_sort(iota(10_000))
 
 
