983085d61fe138c7c8e6a1429f4eec9d8106f6d1
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2023-05-04 03:36:23 +0200

    better iterative insertion algorithm

--- algorithms.py
+++ algorithms.py
@@ -12,19 +12,18 @@ def recursive_insertion(lst: List, value) -> List:
 
 def iterative_insertion(lst: List, value) -> List:
     new_list = List(True)  # List containing a foo value
-    tail = new_list # Last node of the list
-    value_inserted = False  # is the value already inserted ?
-    for elt in lst:
-        if (not value_inserted) and value < elt:
-            tail.next = List(value)
-            tail = tail.next
-            value_inserted = True  # now the value has bee inserted
-        tail.next = List(elt)
+    tail = new_list  # Last node of the list
+    lst_ptr = lst  # pointer to the current element of lst
+    # /!\ order of conditions in the while is important :
+    # you need to test that lst_ptr is not empty before getting its value.
+    while (not lst_ptr.is_empty()) and (value > lst_ptr.value):
+        tail.next = List(lst_ptr.value)
         tail = tail.next
-    if not value_inserted:
-        tail.next = List(value)
-    new_list = new_list.next  # remove the useless first element
-    return List(new_list)
+        lst_ptr = lst_ptr.next
+    tail.next = List(value)
+    tail.next.next = lst_ptr
+    return new_list.cdr()
+
 
 def insertion_sort(insertion_function: Callable[List, List], lst: List) -> List:
     sorted_lst = List(1)
@@ -51,6 +50,7 @@ def time_recursive_insertion_sort(sample: List) -> float:
 A = random_list(10)
 # print(recursive_insertion_sort(A))
 # print(A)
+# print(iterative_insertion(iota(5), 6))
 print(is_sorted(iterative_insertion_sort(A)))
 
 
