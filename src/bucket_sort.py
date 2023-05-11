cee5cc02d1af5cd36f9f2f5583b1ef5e5bbd729c
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2023-05-11 12:22:07 +0200

    auto detect the max value

--- src/bucket_sort.py
+++ src/bucket_sort.py
@@ -2,17 +2,31 @@ from linked_list import List
 from utils import *
 
 
-def bucket_sort(lst: List, max_value: int =100):
+def bucket_sort(lst: List, max_value: int =None):
     """# {{{
+    Args:
+        lst (List): The list to sort. Must be a list of positive integers.
+        max_value (int): The maximum integer in lst. It will be detected
+            automatically if not specified.
     Tests:
         >>> all(is_sorted(bucket_sort(random_list(n))) for n in range(50))
         True
     """# }}}
     if lst.is_empty():
         return List()
+    # find `max_value` : the maximum of the list
+    if max_value is None:
+        max_value = lst.value
+        for elt in lst.cdr():
+            if elt > max_value:
+                max_value = elt
+            if elt < 0:
+                raise ValueError("Negative values not supported.")
+    # initialize the list of buckets (list of zeroes)
     bucket = List()
     for _ in range(max_value+1):
         bucket.prepend(0)
+    # loop over the elements
     for elt in lst:
         if elt > max_value:
             raise ValueError("value not in the bounds of the bucket.")
