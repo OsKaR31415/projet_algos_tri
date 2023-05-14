fc9914ff4dd8a1a04f7ae6e8723f7fde8b7dd670
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2023-05-14 22:36:07 +0200

    Create quick_sort.py

---
+++ src/quick_sort.py
@@ -0,0 +1,44 @@
+from linked_list import List
+from utils import *
+
+
+def quick_sort(lst: List, depth=1):
+    """# {{{
+    Tests:
+        >>> all(is_sorted(quick_sort(random_list(n))) for n in range(50))
+        True
+    """# }}}
+    if lst.is_empty():
+        return lst
+    if lst.next.is_empty():
+        return List(lst.value)
+    pivot = lst.value
+    Linf = List()  # list of elements smaller to the pivot
+    Lsup = List()  # list of elements greater than the pivot
+    Leq = List()  # list of elements equal to the pivot
+    for elt in lst:
+        if elt == pivot:
+            Leq.prepend(elt)
+        elif elt < pivot:
+            Linf.prepend(elt)
+        else:
+            Lsup.prepend(elt)
+    Linf = quick_sort(Linf)
+    Lsup = quick_sort(Lsup)
+    Linf.extend(Leq)
+    Linf.extend(Lsup)
+    return Linf
+
+
+if __name__ == '__main__':
+    from random import randint
+    counter = 0
+    try:
+        while True:
+            counter += 1
+            L = random_list(randint(0, 500))
+            L = quick_sort(L)
+            if not is_sorted(L):
+                break
+    except KeyboardInterrupt:
+        print("\nsorted", counter, "Lists")
