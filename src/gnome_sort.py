86a4e35e037f8469ff241648cfb95f15a9b9f2cc
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2023-05-15 04:29:22 +0200

    comment iterative_gnome_sort

--- src/gnome_sort.py
+++ src/gnome_sort.py
@@ -24,38 +24,36 @@ def recursive_gnome_sort(lst: List, rest: List =None):
         rest.prepend(lst.car())
         return recursive_gnome_sort(lst.cdr(), rest)
 
-def iterative_gnome_sort(lst: List):
-    """# {{{
-    Tests:
-        >>> all(is_sorted(iterative_gnome_sort(random_list(n))) for n in range(50))
-        True
-    """# }}}
-    if lst.is_empty():
-        return List()
-    rest = List(lst.value)
-    lst = lst.cdr()
-    while not lst.is_empty():
-        input(str(list(reversed(list(lst)))) + "  " + str(rest))
-        if rest.is_empty() or lst.value > rest.value:
-            lst.value, rest.value = rest.value, lst.value
-            rest.prepend(lst.car())
-            lst = lst.next
-        else:
-            lst.prepend(rest.car())
-            rest = rest.next
-
-    return rest, lst
+# def iterative_gnome_sort(lst: List):
+#     """# {{{
+#     Tests:
+#         >>> all(is_sorted(iterative_gnome_sort(random_list(n))) for n in range(50))
+#         True
+#     """# }}}
+#     if lst.is_empty():
+#         return List()
+#     rest = List(lst.value)
+#     lst = lst.cdr()
+#     while not lst.is_empty():
+#         input(str(list(reversed(list(lst)))) + "  " + str(rest))
+#         if rest.is_empty() or lst.value > rest.value:
+#             lst.value, rest.value = rest.value, lst.value
+#             rest.prepend(lst.car())
+#             lst = lst.next
+#         else:
+#             lst.prepend(rest.car())
+#             rest = rest.next
+#     return rest, lst
 
 
 @return_execution_time
 def time_recursive_gnome_sort(sample: List) -> float:
     recursive_gnome_sort(sample)
 
-@return_execution_time
-def time_iterative_gnome_sort(sample: List) -> float:
-    iterative_gnome_sort(sample)
+# @return_execution_time
+# def time_iterative_gnome_sort(sample: List) -> float:
+#     iterative_gnome_sort(sample)
 
 if __name__ == "__main__":
-    L = random_list(20)
-    print(iterative_gnome_sort(L))
+    ...
 
